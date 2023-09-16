7e094aa0345c866fe4dcdb98a7f11c10e0682e1d
Jamielloyduni <1910259@swansea.ac.uk>
2023-09-16 17:57:19 +0100

    Added buttons to navigate between map and list view

--- app/src/main/java/com/example/mastersproject/HomeActivity.kt
+++ app/src/main/java/com/example/mastersproject/HomeActivity.kt
@@ -1,7 +1,10 @@
 package com.example.mastersproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.mastersproject.ui.login.LoginActivity
 
 
 class HomeActivity : AppCompatActivity() {
@@ -9,11 +12,19 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
-        /* Set click listeners for each ImageView (button) PAGES DO NOT EXIST YET
+
+        val mapButton = findViewById<Button>(R.id.mapButton)
+        mapButton.setOnClickListener {
+            val intent = Intent(this@HomeActivity, MapHomeActivity::class.java)
+            startActivity(intent)
+
+            /* Set click listeners for each ImageView (button) PAGES DO NOT EXIST YET
         profileButton.setOnClickListener { navigateToProfilePage() }
         homeButton.setOnClickListener { navigateToHomePage() }
         settingsButton.setOnClickListener { navigateToSettingsPage() }
-    }
+
+
+
 
     private fun navigateToProfilePage() {
         // Replace ProfileActivity with the actual activity you want to navigate to
@@ -34,6 +45,7 @@ class HomeActivity : AppCompatActivity() {
     }
             adding notes blah blah blah tes test test
          */
-    }
+        }
 
+    }
 }
---
+++ app/src/main/java/com/example/mastersproject/MapHomeActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mastersproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MapHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_home)
+
+        val ListButton = findViewById<Button>(R.id.ListButton)
+        ListButton.setOnClickListener {
+            val intent = Intent(this@MapHomeActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
