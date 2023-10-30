e2cb30fdded50dcb2a769a54b0e1432aa38fbc3a
Jamielloyduni <1910259@swansea.ac.uk>
2023-10-30 18:18:06 +0000

    added logout button

--- app/src/main/java/com/example/mastersproject/ProfileActivity.kt
+++ app/src/main/java/com/example/mastersproject/ProfileActivity.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.example.mastersproject.ui.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileActivity : AppCompatActivity() {
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -22,5 +26,15 @@ class ProfileActivity : AppCompatActivity() {
             val intent = Intent(this@ProfileActivity, MapTest::class.java)
             startActivity(intent)
         }
+
+
+        val logOut = findViewById<ImageView>(R.id.logout)
+        logOut.setOnClickListener {
+            auth.signOut() // This logs the user out
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
