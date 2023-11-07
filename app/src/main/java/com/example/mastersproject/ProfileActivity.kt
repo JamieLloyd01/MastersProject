057f1c055f4c2622cc8474333d40edd10b5c206f
Jamielloyduni <1910259@swansea.ac.uk>
2023-11-07 15:13:06 +0000

    minor change

--- app/src/main/java/com/example/mastersproject/ProfileActivity.kt
+++ app/src/main/java/com/example/mastersproject/ProfileActivity.kt
@@ -1,15 +1,21 @@
 package com.example.mastersproject
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
 import com.example.mastersproject.ui.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ProfileActivity : AppCompatActivity() {
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -35,6 +41,25 @@ class ProfileActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        // Assuming you have already initialized db as FirebaseFirestore instance
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            val uid = user.uid
+            val docRef = db.collection("users").document(uid)
+            docRef.get().addOnSuccessListener { document ->
+                if (document != null) {
+                    val username = document.getString("username")
+                    val usertitle = findViewById<TextView>(R.id.username)
+                    usertitle.text = username
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }.addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
+        }
+
 
     }
 }
