0b6945e195ef9d3868c5e61cf2cdc3f9fe7e5f81
Jamielloyduni <1910259@swansea.ac.uk>
2023-11-23 20:02:57 +0000

    tidying

--- app/src/main/java/com/example/mastersproject/mapDetailView.kt
+++ app/src/main/java/com/example/mastersproject/mapDetailView.kt
@@ -2,8 +2,6 @@ package com.example.mastersproject
 
 import android.content.Intent
 import android.graphics.Color
-import android.graphics.PorterDuff
-import android.graphics.Typeface
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -19,7 +17,7 @@ import com.squareup.picasso.Picasso
 
 class MapDetailView : Fragment() {
 
-    fun View.bringToBack() {
+    private fun View.bringToBack() {
         val parent = this.parent as ViewGroup
         val index = parent.indexOfChild(this)
         if (index > 0) {
@@ -29,17 +27,14 @@ class MapDetailView : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val rootView = inflater.inflate(R.layout.item_view, container, false)
 
-
-        // Retrieve the selected item from the arguments bundle
         val item = arguments?.getParcelable<Item>("selectedItem")
 
-        // Find views within the inflated layout
         val name: TextView = rootView.findViewById(R.id.activityName)
         val filter1: TextView = rootView.findViewById(R.id.filter1)
         val filter2: TextView = rootView.findViewById(R.id.filter2)
@@ -48,7 +43,6 @@ class MapDetailView : Fragment() {
         val image: ImageView = rootView.findViewById(R.id.image)
         val link: TextView = rootView.findViewById(R.id.visitWebsite)
 
-        // Populate the views with item details
         item?.let {
             name.text = it.name
             filter1.text = it.filter1
@@ -56,95 +50,109 @@ class MapDetailView : Fragment() {
             description.text = it.description
             priceBracket.text = it.priceBracket
 
-            // Load an image using a library like Picasso or Glide
-            if(it.imageurl != null && !it.imageurl!!.isEmpty()) {
+            if (it.imageurl != null) {
                 Picasso.get().load(it.imageurl).into(image)
             }
 
             val linearLayout: LinearLayout = rootView.findViewById(R.id.activityCard)
             val backgroundImage: ImageView = rootView.findViewById(R.id.backgroundImage)
             backgroundImage.bringToBack()
-            val myTextView: TextView = rootView.findViewById(R.id.activityName)
 
-
-            // Set the background image for "Nature & Wildlife"
-            if (it.filter1 == "Nature & Wildlife") {
-                backgroundImage.setImageResource(R.drawable.nature2)
-                myTextView.typeface = ResourcesCompat.getFont(myTextView.context, R.font.amaticboldfont)
-                name.setTextSize(40.0F)
-            } else  if (it.filter1 == "Games") {
-                backgroundImage.setImageResource(R.drawable.games)
-                myTextView.typeface = ResourcesCompat.getFont(myTextView.context, R.font.bangersfont)
-                name.setTextSize(30.0F)
-            } else  if (it.filter1 == "Outdoors") {
-                backgroundImage.setImageResource(R.drawable.outdoors)
-                myTextView.typeface = ResourcesCompat.getFont(myTextView.context, R.font.rustlemonfont)
-                name.setTextSize(26.5F)
-            } else  if (it.filter1 == "Sports & Exercise") {
-                backgroundImage.setImageResource(R.drawable.sports)
-                myTextView.typeface = ResourcesCompat.getFont(myTextView.context, R.font.graduatefont)
-                name.setTextSize(24.5F)
-            } else  if (it.filter1 == "Historic") {
-                backgroundImage.setImageResource(R.drawable.history2)
-                myTextView.typeface = ResourcesCompat.getFont(myTextView.context, R.font.trajanproboldfont)
-                name.setTextSize(25.0F)
-            } else  if (it.filter1 == "Arts") {
-                backgroundImage.setImageResource(R.drawable.art)
-                myTextView.typeface = ResourcesCompat.getFont(myTextView.context, R.font.anandablackfont)
-                name.setTextSize(29.0F)
-            }
-            // Set the background color based on your conditions
-            var backgroundColor = Color.parseColor("#FF5733")
-            when (it.filter1) {
-                "Sports & Exercise" -> backgroundColor = Color.argb(155, 255, 100, 100)
-                "Games" -> backgroundColor = Color.argb( 220, 128, 0, 128)
-                "Outdoors" -> backgroundColor = Color.argb(155, 135, 206, 250)
-                "Nature & Wildlife" -> backgroundColor = Color.argb(120, 54, 150, 64)
-                "Historic" -> backgroundColor = Color.argb(155, 205, 155, 85)
-                "Arts" -> backgroundColor = Color.argb(155, 0, 206, 209)
-                else -> Color.parseColor("#000000")
-            }
-            linearLayout.setBackgroundColor(backgroundColor)
-
-            // Set text colors based on the background color
-            if (backgroundColor == Color.argb(220, 128, 0, 128)) {
-                name.setTextColor(Color.WHITE) // Set text color to white
-                description.setTextColor(Color.WHITE)
-                priceBracket.setTextColor(Color.WHITE)
-            } else if (backgroundColor == Color.argb(255, 255, 100, 100)) {
-                name.setTextColor(Color.argb(255, 0, 0, 0))
-                description.setTextColor(Color.argb(255, 0, 0, 0))
-                priceBracket.setTextColor(Color.argb(255, 0, 0, 0))
-            } else if (backgroundColor == Color.argb(255, 143, 143, 86)) {
-                name.setTextColor(Color.argb(255, 0, 0, 0))
-                description.setTextColor(Color.argb(255, 0, 0, 0))
-                priceBracket.setTextColor(Color.argb(255, 0, 0, 0))
-
-            } else if (backgroundColor == Color.argb(255, 0, 206, 209)) {
-                name.setTextColor(Color.argb(255, 0, 0, 0))
-                description.setTextColor(Color.argb(255, 0, 0, 0))
-                priceBracket.setTextColor(Color.argb(255, 0, 0, 0))
-            } else {
-                // Set text colors for other background colors
-                // Adjust the text colors as needed for each case
-                name.setTextColor(Color.BLACK)
-                description.setTextColor(Color.BLACK)
-                priceBracket.setTextColor(Color.BLACK)
-            }
+            setLayoutDetails(it, name, description, priceBracket, backgroundImage, linearLayout)
         }
 
-
-        // Set an OnClickListener for the "Visit Website" TextView if needed
         link.setOnClickListener {
-            val itemLink = item?.link
-            if (!itemLink.isNullOrBlank()) {
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(itemLink))
-                startActivity(intent)
+            item?.link?.let { itemLink ->
+                if (itemLink.isNotBlank()) {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(itemLink))
+                    startActivity(intent)
+                }
             }
         }
-
 
         return rootView
+    }
+
+    private fun setLayoutDetails(
+        item: Item,
+        name: TextView,
+        description: TextView,
+        priceBracket: TextView,
+        backgroundImage: ImageView,
+        linearLayout: LinearLayout
+    ) {
+        when (item.filter1) {
+            "Nature & Wildlife" -> configureLayoutForNature(name, backgroundImage)
+            "Games" -> configureLayoutForGames(name, backgroundImage)
+            "Outdoors" -> configureLayoutForOutdoors(name, backgroundImage)
+            "Sports & Exercise" -> configureLayoutForSports(name, backgroundImage)
+            "Historic" -> configureLayoutForHistoric(name, backgroundImage)
+            "Arts" -> configureLayoutForArts(name, backgroundImage)
+
+        }
+
+        // Set the background color
+        val backgroundColor = when (item.filter1) {
+            "Sports & Exercise" -> Color.argb(155, 255, 100, 100)
+            "Games" -> Color.argb( 220, 128, 0, 128)
+            "Outdoors" -> Color.argb(155, 135, 206, 250)
+            "Nature & Wildlife" -> Color.argb(120, 54, 150, 64)
+            "Historic" -> Color.argb(155, 205, 155, 85)
+            "Arts" -> Color.argb(155, 0, 206, 209)
+            else -> Color.parseColor("#000000")
+        }
+        linearLayout.setBackgroundColor(backgroundColor)
+
+        // Set text colors
+        when (backgroundColor) {
+            Color.argb(220, 128, 0, 128) -> setColorForViews(name, description, priceBracket, Color.WHITE)
+            Color.argb(255, 255, 100, 100),
+            Color.argb(255, 143, 143, 86),
+            Color.argb(255, 0, 206, 209) -> {
+                name.setTextColor(Color.argb(255, 0, 0, 0))
+                description.setTextColor(Color.argb(255, 0, 0, 0))
+                priceBracket.setTextColor(Color.argb(255, 0, 0, 0))
+            }
+            else -> setColorForViews(name, description, priceBracket, Color.BLACK)
+        }
+    }
+
+    private fun configureLayoutForNature(name: TextView, backgroundImage: ImageView) {
+        backgroundImage.setImageResource(R.drawable.nature2)
+        name.typeface = ResourcesCompat.getFont(name.context, R.font.amaticboldfont)
+        name.textSize = 40.0F
+    }
+
+    private fun configureLayoutForGames(name: TextView, backgroundImage: ImageView) {
+        backgroundImage.setImageResource(R.drawable.games)
+        name.typeface = ResourcesCompat.getFont(name.context, R.font.bangersfont)
+        name.textSize = 30.0F
+    }
+    private fun configureLayoutForOutdoors(name: TextView, backgroundImage: ImageView) {
+        backgroundImage.setImageResource(R.drawable.outdoors)
+        name.typeface = ResourcesCompat.getFont(name.context, R.font.rustlemonfont)
+        name.textSize = 26.5F
+    }
+    private fun configureLayoutForSports(name: TextView, backgroundImage: ImageView) {
+        backgroundImage.setImageResource(R.drawable.sports)
+        name.typeface = ResourcesCompat.getFont(name.context, R.font.graduatefont)
+        name.textSize = 24.5F
+    }
+    private fun configureLayoutForHistoric(name: TextView, backgroundImage: ImageView) {
+        backgroundImage.setImageResource(R.drawable.history2)
+        name.typeface = ResourcesCompat.getFont(name.context, R.font.trajanproboldfont)
+        name.textSize = 25.0F
+    }
+    private fun configureLayoutForArts(name: TextView, backgroundImage: ImageView) {
+        backgroundImage.setImageResource(R.drawable.art)
+        name.typeface = ResourcesCompat.getFont(name.context, R.font.anandablackfont)
+        name.textSize = 29.0F
+    }
+
+    private fun setColorForViews(view: TextView, view2: TextView, view3: TextView, color: Int) {
+        view.setTextColor(color)
+        view2.setTextColor(color)
+        view3.setTextColor(color)
     }
 }
 
